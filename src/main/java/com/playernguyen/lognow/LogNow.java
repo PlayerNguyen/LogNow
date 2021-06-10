58090103d41d7787b34bd987195c5603e1c6684e
PlayerNguyen <daudaua3782@gmail.com>
2021-06-10 14:39:17 +0700

    sqlite added

--- src/main/java/com/playernguyen/lognow/LogNow.java
+++ src/main/java/com/playernguyen/lognow/LogNow.java
@@ -1,26 +1,58 @@
 package com.playernguyen.lognow;
 
+import java.sql.SQLException;
+
 import com.playernguyen.lognow.setting.SettingConfiguration;
+import com.playernguyen.lognow.setting.SettingConfigurationModel;
+import com.playernguyen.pndb.sql.DatabaseOptions;
+import com.playernguyen.pndb.sql.adapt.DatabaseAdaptor;
+import com.playernguyen.pndb.sql.hoster.DatabaseHoster;
+import com.playernguyen.pndb.sql.query.DatabaseQueryBuilder;
+import com.playernguyen.pndb.sql.sqlite.DatabaseHosterSQLite;
+import com.playernguyen.pndb.sql.sqlite.DatabaseOptionsSQLite;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LogNow extends JavaPlugin {
 
     private SettingConfiguration settingConfiguration;
+    private DatabaseHoster databaseHoster;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         try {
             setupSetting();
-            // setupDatabase();
+            setupDatabase();
         } catch (Exception e) {
             // catch all setup error
             e.printStackTrace();
         }
     }
-    
-    
+
+    private void setupDatabase() throws SQLException {
+        // database type fetch
+        String databaseType = settingConfiguration.get(SettingConfigurationModel.DATABASE_TYPE).asString();
+        // sqlite set up
+        if (databaseType.toLowerCase().equalsIgnoreCase("sqlite")) {
+            DatabaseOptions option = new DatabaseOptionsSQLite(
+                    this.getSettingConfiguration().get(SettingConfigurationModel.DATABASE_SQLITE_FILE_NAME).asString());
+            this.databaseHoster = new DatabaseHosterSQLite(option);
+
+            // connection test, failed will throw exception
+            if (this.getDatabaseHoster().connection() == null) {
+                throw new IllegalStateException("cannot generate connection...");
+            }
+
+            // set up table
+            DatabaseQueryBuilder.newInstance(this.getDatabaseHoster()).executeCustomUpdate(
+                    "CREATE TABLE IF NOT EXISTS lognow_user (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+                            + "user_id VARCHAR NOT NULL, password VARCHAR NOT NULL, email VARCHAR NOT NULL)");
+
+        }
+
+    }
+
     private void setupSetting() throws Exception {
         this.getLogger().info("Loading setting configuration from file...");
         if (this.settingConfiguration == null) {
@@ -33,5 +65,20 @@ public final class LogNow extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    /**
+     * 
+     * @return setting configuration instance
+     */
+    public SettingConfiguration getSettingConfiguration() {
+        return settingConfiguration;
+    }
+
+    /**
+     * @return database hoster instance
+     */
+    public DatabaseHoster getDatabaseHoster() {
+        return databaseHoster;
     }
 }
--- src/main/java/com/playernguyen/lognow/setting/SettingConfigurationModel.java
+++ src/main/java/com/playernguyen/lognow/setting/SettingConfigurationModel.java
@@ -4,8 +4,17 @@ import com.playernguyen.lognow.configuration.FileConfigurationModel;
 
 public enum SettingConfigurationModel implements FileConfigurationModel {
 
-	DEVELOPMENT_MODE("DevelopmentMode.Enable", false, "Enhancing the code by using development tools of this plugin.|"
-			+ "This option will leak your information such as |" + "your database configuration, so be careful");
+	DEVELOPMENT_MODE("DevelopmentMode.Enable", false,
+			"Enhancing the code by using development tools of this plugin.|"
+					+ "This option will leak your information such as |" + "your database configuration, so be careful"),
+
+	DATABASE_TYPE("Database.Type", "sqlite", "Your database type that you want to store ", "credential information.",
+			" available: sqlite, mysql"),
+
+	DATABASE_SQLITE_FILE_NAME("Database.SQLiteFileName", "storage.sqlite",
+			"Database file name when the Database.Type set to sqlite.")
+
+	;
 
 	private final String key;
 	private final Object value;
@@ -15,6 +24,12 @@ public enum SettingConfigurationModel implements FileConfigurationModel {
 		this.key = key;
 		this.value = value;
 		this.comments = comments;
+	}
+
+	SettingConfigurationModel(String key, Object value, String... comments) {
+		this.key = key;
+		this.value = value;
+		this.comments = String.join("|", comments);
 	}
 
 	@Override
